df9e62b530bd5f9c50da173215f3f50b0b41255e
NeilKey <neil.key@bluewin.ch>
2014-05-03 19:46:19 +0200

    Init project

---
+++ ChatServerForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChatServer3AC
+{
+    public partial class ChatServerForm : Form
+    {
+        public ChatServerForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
